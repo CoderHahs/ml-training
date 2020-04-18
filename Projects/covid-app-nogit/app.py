c4f085fc9a0b95aa9624609667c699584ab2b834
Hrithik Shah <hrithikchamp@gmail.com>
2020-04-18 00:43:24 -0400

    app

--- Projects/covid-app-nogit/app.py
+++ Projects/covid-app-nogit/app.py
@@ -16,39 +16,11 @@ app.title = 'COVID-19 Report'
 
 server = app.server
 
-# pull all the data
-r = requests.get("https://api.covid19api.com/all")
-data = r.json()
-
+# summary data
 r = requests.get("https://api.covid19api.com/summary")
 summary_data = r.json()
-
-# create dataframe
-df = pd.DataFrame(data)
 summary_df = pd.DataFrame(summary_data['Countries'])
 summary_df = summary_df.sort_values(by=['TotalConfirmed'], ascending=False).drop(columns=['Slug'])
-
-if isinstance(df, (pd.DatetimeIndex, pd.MultiIndex)):
-	df = df.to_frame(index=False)
-
-# remove any pre-existing indices for ease of use in the D-Tale code, but this is not required
-df = df.reset_index().drop('index', axis=1, errors='ignore')
-df.columns = [str(c) for c in df.columns]  # update columns to strings in case they are numbers
-chart_data = pd.concat([
-	df['Date'],
-	df['Confirmed'],
-	df['Deaths'],
-	df['Recovered'],
-	df['Active'],
-	df['Country'],
-], axis=1)
-chart_data = chart_data.sort_values(['Country', 'Date'])
-chart_data = chart_data.groupby(['Country', 'Date']).sum().reset_index()
-chart_data = chart_data.dropna()
-worldwide_grouped = chart_data.groupby(['Date']).sum().reset_index()
-worldwide_grouped['Country'] = 'Worldwide'
-chart_data = pd.concat([chart_data, worldwide_grouped], ignore_index=True)
-countries = chart_data['Country'].unique()
 summary_table = go.Figure(data=[go.Table(
     header=dict(values=['Country', 'NewConfirmed', 'TotalConfirmed', 'NewDeaths', 'TotalDeaths', 'NewRecovered', 'TotalRecovered'],
                 align='left'),
@@ -57,13 +29,26 @@ summary_table = go.Figure(data=[go.Table(
                align='left'))
 ])
 
+# get list of countries
+r = requests.get("https://api.covid19api.com/countries")
+countries = pd.DataFrame(r.json()).sort_values(by='Country')
+
+# initial chart pulling
+r = requests.get('http://ipinfo.io/json')
+iso = r.json()['country']
+slug = countries[countries['ISO2'] == iso]['Slug'].values[0]
+ip_country = countries[countries['ISO2'] == iso]['Country'].values[0]
+r = requests.get('https://api.covid19api.com/country/'+slug)
+initial_data = pd.DataFrame(r.json())[['Date', 'Confirmed', 'Deaths', 'Recovered']].groupby('Date').sum().reset_index()
+
+# world totals
+r = requests.get("https://api.covid19api.com/world/total")
+world_data = r.json()
 
 app.layout = html.Div(children=[
     html.H1(
         children='COVID-19 Report',
-        style={
-            'textAlign': 'center',
-            }
+        style={'textAlign': 'center', 'font-size': '40px'}
     ),
 
     html.Div(
@@ -73,40 +58,65 @@ app.layout = html.Div(children=[
             'textAlign': 'center',
         }
     ),
+	html.Div([
+            html.H2("World - ", style ={'display':'inline', 'font-size': '30px'}),
+			html.H2("Total: "+str(world_data['TotalConfirmed']), style ={'display':'inline'}),
+			html.H2("\tActive: "+str(world_data['TotalConfirmed'] - world_data['TotalDeaths']- world_data['TotalRecovered']), style ={'display':'inline', 'color': 'blue'}),
+			html.H2("\tDeaths: "+str(world_data['TotalDeaths']), style ={'display':'inline', 'color': 'red'}),
+			html.H2("\tRecovered: "+str(world_data['TotalRecovered']), style ={'display':'inline', 'color': 'green'}),
+    	],
+		style={'textAlign': 'center', 'padding':'10px 0px 10px 0px'}
+	),
     html.Div([
             dcc.Dropdown(
                 id='country-selector',
-                options=[{'label': i, 'value': i} for i in countries],
-                value='Worldwide'
+                options=[{'label': i, 'value': i} for i in countries['Country']],
+                value=ip_country
             )
     ]),
+	html.Div([
+        dcc.Graph(
+            id='active-graph',
+            figure={
+                'data': [
+                    {'x': initial_data['Date'], 'y': initial_data['Confirmed']- initial_data['Deaths']-initial_data['Recovered'], 'type': 'line'},
+                ],
+                'layout': dict(
+                    title='Active Cases - '+ip_country,
+                    xaxis={'title': 'Date'},
+                    yaxis={'title': 'Number of people still infected'},
+                )
+            }
+        )],
+		style={'maxWidth': '50%', "display": "inline-block"}
+    ),
 	html.Div([
         dcc.Graph(
             id='log-graph',
             figure={
                 'data': [
-                    {'x': worldwide_grouped['Date'], 'y': np.log(worldwide_grouped['Confirmed']- worldwide_grouped['Deaths']-worldwide_grouped['Recovered']), 'type': 'line'},
+                    {'x': initial_data['Date'], 'y': np.log(initial_data['Confirmed']- initial_data['Deaths']-initial_data['Recovered']), 'type': 'line'},
                 ],
                 'layout': dict(
-                    title='Log of Active Cases - Worldwide',
+                    title='Log of Active Cases - '+ip_country,
                     xaxis={'title': 'Date'},
-                    yaxis={'title': 'Rate of people being affected'},
+                    yaxis={'title': 'Rate of people being infected'},
                 )
             }
         )],
-		style={'maxWidth': '50%', "display": "block","margin-left": "auto","margin-right": "auto"}
+		style={'maxWidth': '50%', "display": "inline-block"}
     ),
     html.Div([
         dcc.Graph(
             id='confirmed-graph',
             figure={
                 'data': [
-                    {'x': worldwide_grouped['Date'], 'y': worldwide_grouped['Confirmed'], 'type': 'line'},
+                    {'x': initial_data['Date'], 'y': initial_data['Confirmed'], 'type': 'line'},
                 ],
                 'layout': dict(
-                    title='Confirmed Cases - Worldwide',
+                    title='Confirmed Cases - '+ip_country,
                     xaxis={'title': 'Date'},
-                    yaxis={'title': 'Number of people affected'},
+                    yaxis={'title': 'Number of total infections'},
                 )
             }
         )],
@@ -117,12 +127,12 @@ app.layout = html.Div(children=[
             id='deaths-graph',
             figure={
                 'data': [
-                    {'x': worldwide_grouped['Date'], 'y': worldwide_grouped['Deaths'], 'type': 'line'},
+                    {'x': initial_data['Date'], 'y': initial_data['Deaths'], 'type': 'line'},
                 ],
                 'layout': dict(
-                    title='Number of Deaths - Worldwide',
+                    title='Number of Deaths - '+ip_country,
                     xaxis={'title': 'Date'},
-                    yaxis={'title': 'Number of people affected'},
+                    yaxis={'title': 'Number of deaths'},
                 )
             }
         )],
@@ -133,12 +143,12 @@ app.layout = html.Div(children=[
             id='recovered-graph',
             figure={
                 'data': [
-                    {'x': worldwide_grouped['Date'], 'y': worldwide_grouped['Recovered'], 'type': 'line'},
+                    {'x': initial_data['Date'], 'y': initial_data['Deaths'], 'type': 'line'},
                 ],
                 'layout': dict(
-                    title='Recovered Cases - Worldwide',
+                    title='Recovered Cases - '+ip_country,
                     xaxis={'title': 'Date'},
-                    yaxis={'title': 'Number of people affected'},
+                    yaxis={'title': 'Number of recovered cases'},
                 )
             }
         )],
@@ -155,63 +165,90 @@ app.layout = html.Div(children=[
     Output('confirmed-graph', 'figure'),
     [Input('country-selector', 'value')])
 def update_confirmed_graph (country_selected):
-    dff = chart_data[chart_data['Country'] == country_selected]
-    return {
+	slug = countries[countries['Country'] == country_selected]['Slug'].values[0]
+	r = requests.get('https://api.covid19api.com/total/country/'+slug)
+	dff = pd.DataFrame(r.json())[['Date', 'Confirmed', 'Deaths', 'Recovered']].groupby('Date').sum().reset_index()
+	return {
         'data': [
             {'x': dff['Date'], 'y': dff['Confirmed'], 'type': 'line'},
         ],
         'layout': dict(
             title='Confirmed Cases - '+ country_selected,
             xaxis={'title': 'Date'},
-            yaxis={'title': 'Number of people affected'},
+            yaxis={'title': 'Number of total infections'},
         )
     }
 
 @app.callback(
     Output('deaths-graph', 'figure'),
     [Input('country-selector', 'value')])
-def update_confirmed_graph (country_selected):
-    dff = chart_data[chart_data['Country'] == country_selected]
-    return {
+def update_deaths_graph (country_selected):
+	slug = countries[countries['Country'] == country_selected]['Slug'].values[0]
+	r = requests.get('https://api.covid19api.com/total/country/'+slug)
+	dff = pd.DataFrame(r.json())[['Date', 'Deaths']].groupby('Date').sum().reset_index()
+	return {
         'data': [
             {'x': dff['Date'], 'y': dff['Deaths'], 'type': 'line'},
         ],
         'layout': dict(
             title='Number of Deaths - '+ country_selected,
             xaxis={'title': 'Date'},
-            yaxis={'title': 'Number of people affected'},
+            yaxis={'title': 'Number of deaths'},
         )
     }
 
 @app.callback(
     Output('recovered-graph', 'figure'),
     [Input('country-selector', 'value')])
-def update_confirmed_graph (country_selected):
-    dff = chart_data[chart_data['Country'] == country_selected]
-    return {
+def update_recovered_graph (country_selected):
+	slug = countries[countries['Country'] == country_selected]['Slug'].values[0]
+	r = requests.get('https://api.covid19api.com/total/country/'+slug)
+	dff = pd.DataFrame(r.json())[['Date', 'Recovered']].groupby('Date').sum().reset_index()
+
+	return {
         'data': [
             {'x': dff['Date'], 'y': dff['Recovered'], 'type': 'line'},
         ],
         'layout': dict(
             title='Recovered Cases - '+ country_selected,
             xaxis={'title': 'Date'},
-            yaxis={'title': 'Number of people affected'},
+            yaxis={'title': 'Number of recovered cases'},
         )
     }
 
 @app.callback(
     Output('log-graph', 'figure'),
     [Input('country-selector', 'value')])
-def update_confirmed_graph (country_selected):
-    dff = chart_data[chart_data['Country'] == country_selected]
-    return {
+def update_log_graph (country_selected):
+	slug = countries[countries['Country'] == country_selected]['Slug'].values[0]
+	r = requests.get('https://api.covid19api.com/total/country/'+slug)
+	dff = pd.DataFrame(r.json())[['Date', 'Confirmed', 'Deaths', 'Recovered']].groupby('Date').sum().reset_index()
+	return {
         'data': [
             {'x': dff['Date'], 'y': np.log(dff['Confirmed']-dff['Deaths']-dff['Recovered']), 'type': 'line'},
         ],
         'layout': dict(
             title='Log of Active Cases - '+country_selected,
 			xaxis={'title': 'Date'},
-			yaxis={'title': 'Rate of people being affected'},
+			yaxis={'title': 'Rate of people being infected'},
+        )
+    }
+
+@app.callback(
+    Output('active-graph', 'figure'),
+    [Input('country-selector', 'value')])
+def update_active_graph (country_selected):
+	slug = countries[countries['Country'] == country_selected]['Slug'].values[0]
+	r = requests.get('https://api.covid19api.com/total/country/'+slug)
+	dff = pd.DataFrame(r.json())[['Date', 'Confirmed', 'Deaths', 'Recovered']].groupby('Date').sum().reset_index()
+	return {
+        'data': [
+            {'x': dff['Date'], 'y': dff['Confirmed']-dff['Deaths']-dff['Recovered'], 'type': 'line'},
+        ],
+        'layout': dict(
+            title='Active Cases - '+country_selected,
+			xaxis={'title': 'Date'},
+			yaxis={'title': 'Rate of people still infected'},
         )
     }
 
